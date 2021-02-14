d0a8a199d5d6c837fa16013783e996ad19d80593
unknown <500075823@stu.upes.ac.in>
2021-02-14 13:51:49 +0530

    Integrated Serial Data Transfer

--- Arduino/AutomationArduino/AutomationArduino.ino
+++ Arduino/AutomationArduino/AutomationArduino.ino
@@ -1,10 +1,31 @@
-int in1 = 5;
-int in2 = 6;
+#include <SoftwareSerial.h>
+SoftwareSerial s(6, 5);
+int data = 0;
+int in1 = 7;
+int in2 = 8;
 void setup() {
-  pinMode(in1, OUTPUT); 
+  Serial.begin(9600);
+  s.begin(9600);
+  pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
 }
 void loop() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  if (s.available()) {
+    data = s.read();
+    if (data == 1) {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+
+    }
+    else {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+    }
+    Serial.println(data);
+  }
+  else {
+    Serial.println("Serial not available");
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
 }
